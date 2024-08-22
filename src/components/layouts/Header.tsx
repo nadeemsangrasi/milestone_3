9e0ef7d287987c30363741db5b4cf9643e45b825
nadeem <nadeemsangrasi903@gmail.com>
2024-08-22 17:19:06 +0500

    add navbar responsive

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -9,19 +9,19 @@ const Header = () => {
 
   return (
     <Wrapper>
-      <div className="z-50 flex justify-between items-center bg-secondary-black text-white px-4 py-4 mt-2 shadow-2xl  w-[90%]  fixed left-1/2 -translate-x-1/2">
-        <h1 className="font-bold text-2xl text-emerald-300">
-          <Link href={"/"}>EliteEcommerce</Link>
+      <div className="z-50 flex justify-between items-center bg-secondary-black text-white px-2 sm:px-4 py-4 mt-2 shadow-2xl w-full  sm:w-[90%]  fixed left-1/2 -translate-x-1/2">
+        <h1 className="font-bold text-xl sm:text-2xl text-emerald-300">
+          <Link href={"/"}>EliteEcom</Link>
         </h1>
         <div>
-          <ul className="flex gap-4">
-            <li className="text-semibold text-xl  hover:text-emerald-500">
+          <ul className="flex gap-2 sm:gap-4 items-center">
+            <li className="text-semibold text-sm sm:text-xl  hover:text-emerald-500">
               <Link href={"/products"}>Products</Link>
             </li>
-            <li className="text-semibold text-xl hover:text-emerald-500">
+            <li className="text-semibold text-sm sm:text-xl hover:text-emerald-500">
               <Link href={"/blogs"}>Blogs</Link>
             </li>
-            <li className="text-semibold text-2xl hover:text-emerald-500">
+            <li className="text-semibold text-xl sm:text-2xl hover:text-emerald-500">
               <Link href={"/carts"} className="flex gap-1 items-center">
                 <ShoppingCart fontSizeAdjust={20} /> {cartCount}
               </Link>
